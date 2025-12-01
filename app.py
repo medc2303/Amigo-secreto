a98896b4166d39193053a2294dfa94a284c1dc5d
medc2303 <75738536+medc2303@users.noreply.github.com>
2025-12-01 18:27:36 -0300

    app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ if not juego_iniciado:
         input_names = st.text_area(
             "Nombres (uno por línea):",
             height=150,
-            placeholder="Juan\nMaría\nPedro"
+            placeholder="Martin\nDiego\nLucho"
         )
         
         if st.button("🎲 Sortear y Guardar", type="primary"):
@@ -248,3 +248,4 @@ else:
                 st.success("Borrado.")
                 time.sleep(1)
                 st.rerun()
+
